f550307495d07ddf344816e45d054633e076ad15
Allwyen <allwyen27@gmail.com>
2019-08-29 09:35:09 +0530

    updated

--- app.js
+++ app.js
@@ -30,9 +30,11 @@ app.use(function (req, res, next) {
     next();
 });
 
-Mongoose.connect("mongodb+srv://mongodb:mongodb@mycluster-ucvz5.mongodb.net/SMS?retryWrites=true&w=majority");
+//Mongoose.connect("mongodb+srv://mongodb:mongodb@mycluster-ucvz5.mongodb.net/SMS?retryWrites=true&w=majority");
 
-const StudModel = Mongoose.model("studentdetail",{
+Mongoose.connect("mongodb://localhost:27017/SMS");
+
+const StudModel = Mongoose.model("studentdata",{
     sadmno:String,
     srollno:String,
     sname:String,
@@ -43,6 +45,7 @@ const StudModel = Mongoose.model("studentdetail",{
 });
 
 app.post('/insertstud',(req,res)=>{
+    console.log(req.body);
     var stud = new StudModel(req.body);
     var result = stud.save((error,data)=>{
         if(error)
@@ -72,6 +75,59 @@ app.get('/viewstud',(req,res)=>{
     });
 });
 
-app.listen(process.env.PORT || 4000,()=>{
-    console.log("Server running on port::4000...");
+app.post('/searchstud',(req,res)=>{
+    const item = req.body.srollno;
+    var result = StudModel.find({srollno:item},(error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send(data);
+        }
+    });
+});
+
+app.post('/deletestud',(req,res)=>{
+    const item = req.body._id;
+    var result = StudModel.deleteOne({_id:item},(error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send(data);
+        }
+    });
+});
+
+app.post('/updatestud',(req,res)=>{
+    const item = req.body._id;
+    const admno = req.body.sadmno;
+    const rollno = req.body.srollno;
+    const name = req.body.sname;
+    const college = req.body.scollege;
+    const branch = req.body.sbranch;
+    const dob = req.body.sdob;
+    const email = req.body.semail;
+
+    var result = StudModel.update({_id:item},{$set:{sadmno:admno,srollno:rollno,sname:name,scollege:college,sbranch:branch,sdob:dob,semail:email}},(error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send(data);
+        }
+    });
+});
+
+app.listen(process.env.PORT || 4579,()=>{
+    console.log("Server running on port::4579...");
 });
